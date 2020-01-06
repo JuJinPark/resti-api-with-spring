69785a652b467bd950e933d800ddb7aa3e64f766
JuJinPark <wnwls1216@gmail.com>
2020-01-06 17:40:18 +0900

    restirct input for POST /api/events

--- src/main/java/me/jujin/demoinflearnrestapi/DemoApplication.java
+++ src/main/java/me/jujin/demoinflearnrestapi/DemoApplication.java
@@ -1,14 +1,21 @@
 package me.jujin.demoinflearnrestapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication()
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper( ){
+        return new ModelMapper();
     }
 
 }
--- src/main/java/me/jujin/demoinflearnrestapi/events/EventDto.java
+++ src/main/java/me/jujin/demoinflearnrestapi/events/EventDto.java
@@ -1,18 +1,16 @@
 package me.jujin.demoinflearnrestapi.events;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Builder @NoArgsConstructor @AllArgsConstructor
+@Data
+public class EventDto {
 
-@Builder @AllArgsConstructor @NoArgsConstructor
-@Getter @Setter @EqualsAndHashCode(of="id")
-
-@Entity
-public class Event {
-    @Id @GeneratedValue
-    private Integer id;
     private String name;
     private String description;
     private LocalDateTime beginEnrollmentDateTime;
@@ -23,8 +21,5 @@ public class Event {
     private int basePrice; // (optional)
     private int maxPrice; // (optional)
     private int limitOfEnrollment;
-    private boolean offline;
-    private boolean free;
-    @Enumerated(EnumType.STRING)
-    private EventStatus eventStatus = EventStatus.DRAFT;
+
 }
