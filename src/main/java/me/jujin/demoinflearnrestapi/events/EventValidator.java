61f8607f75cf773b3d528528ff3b40d7d64d00b3
JuJinPark <wnwls1216@gmail.com>
2020-01-12 20:19:30 +0900

    add error body response

--- src/main/java/me/jujin/demoinflearnrestapi/events/EventValidator.java
+++ src/main/java/me/jujin/demoinflearnrestapi/events/EventValidator.java
@@ -10,8 +10,8 @@ import java.time.LocalDateTime;
 public class EventValidator {
     public void validate(EventDto eventDto, Errors errors){
         if(eventDto.getBasePrice()>eventDto.getMaxPrice() && eventDto.getMaxPrice() !=0){
-            errors.rejectValue("basePrice","wrongValue","BasePrice is wrong");
-            errors.rejectValue("maxPrice","wrongValue","MaxPrice is wrong");
+
+            errors.reject("wrongPrices","values for prices are wrong");
         }
     LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
         if(endEventDateTime.isBefore(eventDto.getBeginEventDateTime()) ||
